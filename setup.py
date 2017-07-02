1f34f278df1a2c73c572461a177025b201bc0e3b
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2017-07-01 22:06:27 -0400

    New bugfix release.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
   packages = ['py_d3'], # this must be the same as the name above
   install_requires=['ipython'],
   py_modules=['py_d3'],
-  version = '0.2.6',
+  version = '0.2.7',
   description = 'D3 block magic for Jupyter notebook.',
   author = 'Aleksey Bilogur',
   author_email = 'aleksey@residentmar.io',
   url = 'https://github.com/ResidentMario/py_d3',
-  download_url = 'https://github.com/ResidentMario/py_d3/tarball/0.2.6',
+  download_url = 'https://github.com/ResidentMario/py_d3/tarball/0.2.7',
   keywords = ['data', 'data visualization', 'data analysis', 'python', 'jupyter', 'ipython', 'd3', 'javascript'],
   classifiers = []
 )
